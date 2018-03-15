f3f8e05a4ee74f263973cbb0fb7395c5cdfb41df
Kieran Hegarty <kieranheg@users.noreply.github.com>
2018-03-15 23:04:19 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailValue: ''
+    };
+  }
+
+  // updateEmailValue: (evt) => {
+  //   this.setState({
+  //     emailValue: evt.target.value
+  //   });
+  // }
+
+  render() {
+    return (
+      <div className="form-style-10">
+        <header className="App-header">
+          <h1 className="App-title">Aiste Bia</h1>
+        </header>
+        <div className="App-member-details">
+          <div>
+            {/* <label>First name</label>
+            <input type="text" name="firstname"> */}
+            {/* <label>Last name</label>
+              <input type="text" name="lastname"> */}
+          </div>
+          <div className="section">
+            <label>Email Address</label>
+            <div className="inner-wrap">
+              {/* <input value={this.state.emailValue} onChange={evt => this.updateEmailValue(evt)}/> */}
+              <input name="email" type="email" placeholder="email" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
